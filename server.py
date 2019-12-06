6f0aea9ea20d618bbc56af59dd4fb5d484921cc1
melios2 <49787198+melios2@users.noreply.github.com>
2019-12-06 19:03:37 +0200

    Update server.py

--- server.py
+++ server.py
@@ -1,33 +1,34 @@
 import socket, time
 
+print(socket.gethostbyname(socket.gethostname()))
 host = socket.gethostbyname(socket.gethostname())
-port = 9090
+port = 4040
 
 clients = []
 
-s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-s.bind((host,port))
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind((host, port))
 
 quit = False
-print("[ Server Started ]")
+print("[ СЕРВЕР ЗАПУЩЕНО ]")
 
 while not quit:
-	try:
-		data, addr = s.recvfrom(1024)
+    try:
+        data, addr = s.recvfrom(1024)
 
-		if addr not in clients:
-			clients.append(addr)
+        if addr not in clients:
+            clients.append(addr)
 
-		itsatime = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
+        itsatime = time.strftime("%Y-%m-%d-%H.%M.%S", time.localtime())
 
-		print("["+addr[0]+"]=["+str(addr[1])+"]=["+itsatime+"]/",end="")
-		print(data.decode("utf-8"))
+        print("[" + addr[0] + "]=[" + str(addr[1]) + "]=[" + itsatime + "]/", end="")
+        print(data.decode("utf-8"))
 
-		for client in clients:
-			if addr != client:
-				s.sendto(data,client)
-	except:	
-		print("\n[ Server Stopped ]")
-		quit = True
-		
+        for client in clients:
+            if addr != client:
+                s.sendto(data, client)
+    except:
+        print("\n[ СЕРВЕР ВИКЛЮЧЕНО ]")
+        quit = True
+
 s.close()
